f46b08da5e9bd49fba8ac537dab9cf1c5b0aebb0
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-01-23 01:25:06 +0100

    auto

---
+++ build.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const marked = require("marked");
+const path = require("path");
+
+// Define the directory containing the Markdown files
+const directory = "./STROMDAO_EAFs.wiki";
+
+// Get all the Markdown files in the directory
+const files = fs.readdirSync(directory);
+
+// Create a directory for the HTML files
+if(!fs.existsSync("./docs")) {
+    fs.mkdirSync("./docs");
+}
+
+const template = fs.readFileSync("./template.html", "utf8");
+const header = template.substring(0, template.indexOf("##INSERT_CODE##"));
+const footer = template.substring(template.indexOf("##INSERT_CODE##") + 15);
+const myfiles = [];
+// Loop through the Markdown files
+for (const file of files) {
+  try {
+  // Read the Markdown file
+  const markdown = fs.readFileSync(path.join(directory, file), "utf8");
+  // Extract the frontmatter using a regular expression
+  const frontmatterRegex = /^(---\n[\s\S]*?\n---)/;
+  const frontmatter = markdown.match(frontmatterRegex);
+  if(frontmatter !== null) {
+    console.log(frontmatter[0]);
+  }
+
+  // Remove the frontmatter from the Markdown
+  let markdownWithoutFrontmatter = markdown.replace(frontmatterRegex, "");
+  const regex = /^## (.*)/m;
+  let title = file.replace(".md", "")
+  const result = regex.exec(markdownWithoutFrontmatter);
+  if(result !== null) {
+    title = result[1];
+  }
+  //const regex = /^## (.*)/m;
+  markdownWithoutFrontmatter= markdownWithoutFrontmatter.replace(regex, "");
+  // Convert the Markdown to HTML
+  const html = marked.parse(markdownWithoutFrontmatter);
+  let updatedHtml = html.replace(/\[\[(.*?)\]\]/g, "<a href=\"./$1.html\">$1</a>");
+
+  // Create an HTML file with the same name as the Markdown file
+  const htmlFile = path.join("./docs", file.replace(".md", ".html"));
+  myfiles.push( {
+    file:"./docs/"+file.replace(".md", ".html"),
+    title:title
+  })
+
+  
+  let fullHTML = header + updatedHtml + footer;
+  fullHTML= fullHTML.replace(/##TITLE##/g, title);
+
+  // Write the HTML to the file
+  fs.writeFileSync(htmlFile, fullHTML);
+
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+fs.writeFileSync("./docs/files.json", JSON.stringify(myfiles));
